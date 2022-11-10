80a6843e8583245adf2181d3d0fc845316840c7e
18Kailash <sumer.ki359@gmail.com>
2022-11-10 08:51:35 +0530

    update

--- day-2/src/Pages/Men.jsx
+++ day-2/src/Pages/Men.jsx
@@ -1,30 +1,41 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
+import Button from 'react-bootstrap/Button';
 
 
 export default function Men() {
 
+
+    useEffect(()=>{
+          fetch("https://fakestoreapi.com/products")
+            .then((res) => res.json())
+            .then((json) => console.log(json));
+    },[])
+
+
+
+
+
+
   return (
-<Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+     <div>
+        <Card style={{ width: '19rem' }}>
+      <Card.Img variant="top" src="https://img.mytheresa.com/420/475/95/jpeg/catalog/product/0e/P00734009.jpg"/>
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
+        <Card.Title>GABRIELA HEARST</Card.Title>
+       
       </Card.Body>
       <ListGroup className="list-group-flush">
         <ListGroup.Item>Cras justo odio</ListGroup.Item>
-        <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-        <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+        <ListGroup.Item>€ 379</ListGroup.Item>
+        
+        
       </ListGroup>
-      <Card.Body>
-        <Card.Link href="#">Card Link</Card.Link>
-        <Card.Link href="#">Another Link</Card.Link>
-      </Card.Body>
+
+      <Button variant="primary">Add To Cart</Button>
      
-    </Card>
+     </Card>
+     </div>
   )
 }
